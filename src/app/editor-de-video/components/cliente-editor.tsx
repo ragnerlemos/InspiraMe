c6749b6e940317816012c2f198c0b1086eeb8880
Ragner Lemos <efeitosbd@gmail.com>
2025-09-05 13:58:54 +0000

    nao funcinou, vamos tentar outra forma?

--- src/app/editor-de-video/components/cliente-editor.tsx
+++ src/app/editor-de-video/components/cliente-editor.tsx
@@ -283,9 +283,9 @@ export function EditorClient() {
   }
 
   return (
-    <div className="flex flex-col md:flex-row w-full h-full overflow-hidden">
+    <div className="flex flex-col md:flex-row w-full flex-1">
       {/* Área de visualização */}
-      <div className="flex-1 flex justify-center items-start bg-muted/40 p-4 md:p-8 overflow-hidden">
+      <div className="flex-1 flex justify-center items-start bg-muted/40 p-4 md:p-8">
         <VisualizacaoEditor
             aspectRatio={currentState.aspectRatio}
             backgroundStyle={currentState.backgroundStyle}
