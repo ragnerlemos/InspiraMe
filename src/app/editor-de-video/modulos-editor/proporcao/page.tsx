eb912797bcccf090584e92e544a967b43fb7a8b4
Ragner Lemos <efeitosbd@gmail.com>
2025-09-08 16:51:18 +0000

    centrlizado

--- src/app/editor-de-video/modulos-editor/proporcao/page.tsx
+++ src/app/editor-de-video/modulos-editor/proporcao/page.tsx
@@ -105,7 +105,7 @@ export default function AspectWeaver() {
             {/* Canvas for aspect ratio preview */}
             <div
               className={cn(
-                "absolute max-w-full max-h-full transition-all duration-300 ease-in-out shadow-2xl rounded-xl top-[10px] left-[10px] right-[10px]"
+                "absolute m-auto max-w-full max-h-full transition-all duration-300 ease-in-out shadow-2xl rounded-xl inset-[10px]"
               )}
               style={{
                 aspectRatio: aspectRatio,
