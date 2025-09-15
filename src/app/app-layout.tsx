37aa4a312cc5ddb06696492187ff92fc3c7cd1f9
Ragner Lemos <efeitosbd@gmail.com>
2025-09-15 16:14:51 +0000

    ainda nao resolveu

--- src/app/app-layout.tsx
+++ src/app/app-layout.tsx
@@ -22,7 +22,7 @@ export function AppLayout({ children }: { children: React.ReactNode }) {
        ) : (
           <>
             <AppHeader />
-             <div className="flex-1 flex flex-col">
+             <div className="flex-1 flex flex-col min-h-0">
                 {children}
             </div>
           </>
