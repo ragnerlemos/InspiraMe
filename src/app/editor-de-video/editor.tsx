c5c96caa6714b46476731ddededc93b024fb2958
Ragner Lemos <efeitosbd@gmail.com>
2025-09-22 12:47:37 +0000

    ReferenceError: useState is not defined     at Editor (https://6000-fire

--- src/app/editor-de-video/editor.tsx
+++ src/app/editor-de-video/editor.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useWindowSize } from "react-use";
 import { useProfile } from "@/hooks/use-profile";
 import { Sidebar } from "@/app/editor-de-video/components/sidebar";
