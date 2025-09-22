9165c69d08c43545b4b4cac2cd2f05b281a5975d
Ragner Lemos <efeitosbd@gmail.com>
2025-09-22 16:17:18 +0000

    1. Forçar pré-carregamento das fontes no <head>
    No Next.js, adicione no

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,9 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" suppressHydrationWarning className="h-full">
       <head>
-        {/* As fontes agora estão embutidas em globals.css */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&family=PT+Sans:wght@400;700&family=Merriweather:ital,wght@0,400;0,700;1,400&family=Lobster&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased h-full">
         <ThemeProvider
