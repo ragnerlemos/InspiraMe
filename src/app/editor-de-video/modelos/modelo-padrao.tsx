f62ffd1982bf22697ea3b24358ebc4b736704462
Ragner Lemos <efeitosbd@gmail.com>
2025-09-22 13:25:47 +0000

    agor aprecisa de alinhar centrarlizar

--- src/app/editor-de-video/modelos/modelo-padrao.tsx
+++ src/app/editor-de-video/modelos/modelo-padrao.tsx
@@ -31,74 +31,78 @@ export function ModeloPadrao({
   logoOpacity,
 }: ModeloPadraoProps) {
 
-  const getJustifyContentClass = (align: React.CSSProperties['textAlign']) => {
+  const getJustifyClass = (align: React.CSSProperties['textAlign']) => {
     switch (align) {
-      case 'left':
-        return 'justify-start';
-      case 'right':
-        return 'justify-end';
-      default:
-        return 'justify-center';
+      case 'left': return 'justify-start';
+      case 'right': return 'justify-end';
+      default: return 'justify-center';
     }
   };
 
   return (
-    <>
+    <div className="relative w-full h-full p-8 flex flex-col justify-center items-center">
+      
+      {/* Container de Texto Principal */}
       <div 
-        className={cn("absolute inset-x-0 flex p-8", getJustifyContentClass(textStyle.textAlign))}
+        className={cn("w-full flex", getJustifyClass(textStyle.textAlign))}
         style={{
+          position: 'absolute',
           top: `${textVerticalPosition}%`,
-          transform: 'translateY(-50%)',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          padding: '0 2rem', // Adiciona padding para evitar que o texto toque as bordas
         }}
       >
         <div
           style={textStyle}
-          className={cn("break-words w-full transition-all duration-200")}
+          className="break-words"
         >
           {text}
         </div>
       </div>
 
+      {/* Container da Assinatura */}
       {showProfileSignature && (
         <div
-          className="absolute inset-0 flex items-end justify-start pointer-events-none p-4"
+          className="absolute w-full flex justify-center"
           style={{
-            transform: `scale(${signatureScale / 100})`,
-            transformOrigin: `${signaturePositionX}% ${signaturePositionY}%`,
+            top: `${signaturePositionY}%`,
+            left: `${signaturePositionX}%`,
+            transform: `translate(-50%, -50%) scale(${signatureScale / 100})`,
+            transformOrigin: 'center center',
           }}
         >
-          <div className="absolute" style={{ top: `${signaturePositionY}%`, left: `${signaturePositionX}%`, transform: 'translate(-50%, -50%)'}}>
-            <AssinaturaPerfil
-              profile={profile}
-              showPhoto={showSignaturePhoto}
-              showUsername={showSignatureUsername}
-              showSocial={showSignatureSocial}
-              showBackground={showSignatureBackground}
-              bgColor={signatureBgColor}
-              bgOpacity={signatureBgOpacity}
-            />
-          </div>
+          <AssinaturaPerfil
+            profile={profile}
+            showPhoto={showSignaturePhoto}
+            showUsername={showSignatureUsername}
+            showSocial={showSignatureSocial}
+            showBackground={showSignatureBackground}
+            bgColor={signatureBgColor}
+            bgOpacity={signatureBgOpacity}
+          />
         </div>
       )}
 
+      {/* Container da Logo */}
       {showLogo && profile.logo && (
          <div
-          className="absolute inset-0 flex items-end justify-start pointer-events-none p-4"
+          className="absolute w-full flex justify-center"
           style={{
-            transform: `scale(${logoScale / 100})`,
-            transformOrigin: `${logoPositionX}% ${logoPositionY}%`,
+            top: `${logoPositionY}%`,
+            left: `${logoPositionX}%`,
+            transform: `translate(-50%, -50%) scale(${logoScale / 100})`,
+            transformOrigin: 'center center',
             opacity: logoOpacity / 100,
           }}
         >
-            <div className="absolute" style={{ top: `${logoPositionY}%`, left: `${logoPositionX}%`, transform: 'translate(-50%, -50%)'}}>
-              <img
-                src={profile.logo}
-                alt="Logomarca"
-                className="max-w-[150px] max-h-[150px]"
-              />
-            </div>
+            <img
+              src={profile.logo}
+              alt="Logomarca"
+              className="max-w-[150px] max-h-[150px]"
+            />
         </div>
       )}
-    </>
+    </div>
   );
 }
