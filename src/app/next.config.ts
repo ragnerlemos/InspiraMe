610d1225cb7ad1c15ff625fa78db526cf1e754a6
Ragner Lemos <efeitosbd@gmail.com>
2025-09-24 15:18:42 +0000

    Problema principal
    Erro principal:
    
    ⨯ useSearchParams() should be wrapp

--- src/app/next.config.ts
+++ src/app/next.config.ts
@@ -2,10 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
