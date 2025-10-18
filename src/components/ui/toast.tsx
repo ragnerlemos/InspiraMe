3157f1767a315926a52a92ef443f108cd5c7c5fb
Ragner Lemos <efeitosbd@gmail.com>
2025-10-18 14:39:53 +0000

    as notificaçoes ela nao esta fechando depois de um tempo e eu q tenho qu

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -49,6 +49,7 @@ const Toast = React.forwardRef<
     <ToastPrimitives.Root
       ref={ref}
       className={cn(toastVariants({ variant }), className)}
+      duration={5000}
       {...props}
     />
   )
