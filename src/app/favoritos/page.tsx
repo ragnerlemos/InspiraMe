4b03edcd210055473df212b004c78fafbb1391ec
Ragner Lemos <efeitosbd@gmail.com>
2026-01-14 18:39:55 +0000

    vc esqueceu duas pagina para restaurar ,
    * app/layout.tsx
    
    > app/favor

--- src/app/favoritos/page.tsx
+++ src/app/favoritos/page.tsx
@@ -1,28 +1,11 @@
 
-'use client';
-
-import { useEffect } from 'react';
-import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import { FavoritesClientPage } from './favoritos-client';
 import { getAllQuotes } from '@/lib/dados';
-import { Suspense } from 'react';
 
 
-function FavoritesPageContent() {
-  const [allQuotes, setAllQuotes] = useState<any[]>([]);
-
-  useEffect(() => {
-    getAllQuotes().then(quotes => setAllQuotes(quotes));
-  }, []);
-
-  return <FavoritesClientPage allQuotes={allQuotes} />;
-}
-
-export default function FavoritesPage() {
+export default async function FavoritesPage() {
+  const allQuotes = await getAllQuotes();
   return (
-    <Suspense fallback={<div>Carregando...</div>}>
-      <FavoritesPageContent />
-    </Suspense>
+    <FavoritesClientPage allQuotes={allQuotes} />
   );
 }
-
